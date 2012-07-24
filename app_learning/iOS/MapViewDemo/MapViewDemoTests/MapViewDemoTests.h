2abde9fab8233d8677c0b80a675d5edb655ee05e
Xiao Lihao <xiao.tlltrl@gmail.com>
2012-07-24 15:46:14 +0800

    map test

---
+++ app_learning/iOS/MapViewDemo/MapViewDemoTests/MapViewDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  MapViewDemoTests.h
+//  MapViewDemoTests
+//
+//  Created by xiao lihao on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MapViewDemoTests : SenTestCase
+
+@end
---
+++ app_learning/iOS/MapViewDemo/MapViewDemoTests/MapViewDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  MapViewDemoTests.m
+//  MapViewDemoTests
+//
+//  Created by xiao lihao on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapViewDemoTests.h"
+
+@implementation MapViewDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapViewDemoTests");
+}
+
+@end
